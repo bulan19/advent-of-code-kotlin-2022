9231a07f3b44793d56173b315bfd331980210f08
Kristoffer Berglund <kristofferberglund90@gmail.com>
2022-12-11 23:00:37 +0100

    day 1

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,13 +1,24 @@
 fun main() {
-    fun part1(input: List<String>): Int {
-        return input.size
-    }
-
-    fun part2(input: List<String>): Int {
-        return input.size
-    }
-
-    val input = readInput("test")
+    val input = readInput("day01")
     part1(input).println()
     part2(input).println()
+}
+
+fun part1(input: List<String>): Int {
+    return sums(input).max()
+}
+
+fun part2(input: List<String>): Int {
+    return sums(input).sortedDescending().take(3).sum()
+}
+fun sums(lines: List<String>): List<Int> {
+    val sum = mutableListOf<Int>(0)
+    for (line in lines) {
+        if(line.isEmpty()) {
+            sum += 0
+        } else {
+            sum[sum.lastIndex] += line.toInt()
+        }
+    }
+    return sum
 }
